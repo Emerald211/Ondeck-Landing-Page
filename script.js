246d5229bc687b7c7b3bcbcd8c6461f796a4ad8c
Emerald211 <aderibigbeemmanuel2004@gmail.com>
2022-04-19 17:46:50 -1200

    ondeck landing page

---
+++ script.js
@@ -0,0 +1,31 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+  function goTo() {
+      const url = `https://www.freeconferencecall.com/?country_code=ng&locale=global`;
+      window.open(url, "_blank");
+  }
+
+  function myFunction() {
+      document.getElementById('myDropdown').classList.toggle('show')
+  }
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
